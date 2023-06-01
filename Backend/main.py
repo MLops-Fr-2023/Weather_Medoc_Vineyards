7255e3c957db98b95e489d2b8a8ceadccc5c5c14
Joffrey Lemery <joffrey.lemery.jl@gmail.com>
2023-05-31 23:02:23 -0400

    Add of fetch data function and road Miss the snowflake update in the road (Same road of fetch-data)

--- Backend/main.py
+++ Backend/main.py
@@ -1,5 +1,5 @@
 import uvicorn
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta,time
 from typing import Annotated, Optional, Union
 from fastapi.security import OAuth2PasswordBearer, HTTPBasic, HTTPBasicCredentials, OAuth2PasswordRequestForm
 from fastapi import FastAPI, Form, File, Response, Header, UploadFile, Depends, HTTPException, status, Query
@@ -12,12 +12,15 @@ import random
 import json
 import os
 from dotenv import dotenv_values
+import pandas as pd
+import requests
+from snowflake.connector import connect
 
 
 config = {**dotenv_values(".env_API")} # config = {"USER": "foo", "EMAIL": "foo@example.org"}
 print(config)
-########## Param Security ##########
 
+########## Param Security ##########
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
@@ -25,8 +28,20 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 SECRET_KEY = config['SECRET_KEY']
 ALGORITHM = config['ALGORITHM']
 ACCESS_TOKEN_EXPIRE_MINUTES = config['ACCESS_TOKEN_EXPIRE_MINUTES']
+WEATHER_API_KEY = config['WEATHER_API_KEY']
 
-############## dB ##############
+########### Snowflake #############
+
+snowflake_config = {
+    "user": "<your_username>",
+    "password": "<your_password>",
+    "account": "<your_account>",
+    "warehouse": "<your_warehouse>",
+    "database": "<your_database>",
+    "schema": "<your_schema>"
+}
+
+############### dB ###############
 
 
 users_db = {
@@ -35,29 +50,58 @@ users_db = {
         "username": "airflow_train",
         "hashed_password": pwd_context.hash('XXXXX'),
         "position": 'client',
-        "disabled": False,
+        "forecast": False,
+        "get_data": False,
+        "training": True,
+        "disabled": False
     },
 
-    "Aiflow_train" : {
+    "Aiflow_get_data" : {
         "username" :  "airflow_get_data",
         "hashed_password" : pwd_context.hash('XXXXX'),
         "position": 'client',
-        "disabled": False,
+        "forecast": False,
+        "get_data": True,
+        "training": False,
+        "disabled": False
     },
 
     "admax" : {
         "username" :  "admax",
         "hashed_password" : pwd_context.hash('XXXXX'),
         "position": 'Admin',
-        "disabled": True,
+        "forecast": True,
+        "get_data": True,
+        "training": True,
+        "disabled": False
+    },
+
+    "user" : {
+        "username" :  "user",
+        "hashed_password" : pwd_context.hash('XXXXX'),
+        "position": 'Admin',
+        "forecast": True,
+        "get_data": False,
+        "training": False,
+        "disabled": False
     }
+
 }
+
+cities = ['Margaux', 'Soussan', 'Macau, FR', 'Castelnau-de-Medoc','Lamarque, FR', 'Ludon-Medoc', 'Arsac', 'Brach, FR', 'Saint-Laurent Medoc' ]
+
+columns_mandatory = ['observation_time','temperature','weather_code','wind_speed',
+                     'wind_degree','wind_dir','pressure','precip','humidity',
+                     'cloudcover','feelslike','uv_index','visibility','time','city']
 
 ############## Class ##############
 
 class User(BaseModel):
     username: str
     position: Union[str, None] = None
+    forecast: Union[bool, None] = None
+    get_data: Union[bool, None] = None
+    training: Union[bool, None] = None
     disabled: Union[bool, None] = None
 
 class UserInDB(User):
@@ -72,17 +116,6 @@ class TokenData(BaseModel):
 
 class City(BaseModel):
     name_city: Union[str, None] = None
-
-# class Data(BaseModel):
-#     XXXXX: Union[str, None] = None
-#     XXXXX: Union[str, None] = None
-#     XXXXX: Union[str, None] = None
-#     XXXXX: Union[str, None] = None
-#     XXXXX: Union[str, None] = None
-#     XXXXX: Union[str, None] = None
-#     XXXXX: Union[str, None] = None 
-#     XXXXX: Union[str, None] = None 
-
 
 ############## API ##############
 
@@ -111,6 +144,8 @@ description="API for the weather forecasting around Margaux Castle",
         'description': 'Functions related to admins'
     }])
 
+
+######### Security Functions ########
 
 
 def verify_password(plain_password, hashed_password):
@@ -163,8 +198,6 @@ async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
         raise credentials_exception
     return user
 
-
-
 async def get_current_active_user(
     current_user: Annotated[User, Depends(get_current_user)]):
     if current_user.disabled:
@@ -172,7 +205,51 @@ async def get_current_active_user(
     return current_user
 
 
+######### Roads Functions #########
 
+async def fetch_data(columns = columns_mandatory, type_request = 'current', API_KEY = WEATHER_API_KEY):
+    
+    # collect current data and create DataFrame
+    df_current = pd.DataFrame(columns = columns)
+
+    # collect current data and create DataFrame
+    for city in cities:
+        df_tmp = {}
+        params = {
+            'access_key': API_KEY,
+            'query': city 
+        }
+        api_result = requests.get(f'http://api.weatherstack.com/{type_request}', params)
+        response = api_result.json()
+        current = response['current']
+        df_tmp['observation_time'] = response['location']['localtime']
+        df_tmp['temperature'] = current['temperature']
+        df_tmp['weather_code'] = current['weather_code']
+        df_tmp['wind_speed'] = current['wind_speed']
+        df_tmp['wind_degree'] = current['wind_degree']
+        df_tmp['wind_dir'] = current['wind_dir']
+        df_tmp['pressure'] = current['pressure']
+        df_tmp['precip'] = current['precip']
+        df_tmp['humidity'] = current['humidity']
+        df_tmp['cloudcover'] = current['cloudcover']
+        df_tmp['feelslike'] = current['feelslike']
+        df_tmp['uv_index'] = current['uv_index']
+        df_tmp['visibility'] = current['visibility']
+        df_tmp['time'] = current['observation_time']
+        df_tmp['city'] = city
+        df_tmp = pd.DataFrame(data=df_tmp,index=[0])
+        df_current = pd.concat([df_current, df_tmp])
+
+    df_current.reset_index(inplace=True, drop=True)
+
+    # Modify DataFrame to have same strutucre than historical Dataframe
+    for i in range(df_current.shape[0]):
+        x, y = df_current['observation_time'][i].split(' ')
+        df_current['observation_time'][i] = x
+        time = datetime.strptime(df_current['time'][i], "%I:%M %p")
+        df_current['time'][i] = time.strftime('%H:%M')
+
+    return df_current
 
 
 ############## Roads ##############
@@ -213,18 +290,25 @@ async def read_users_me(
 
 
 
-@app.post("/force_data_update",  name='Force weather data update', tags=['Administrators'])
-async def force_data_update(current_user: Annotated[User, Depends(get_current_active_user)]):
+@app.post("/get_data",  name='Force weather data update', tags=['Backend'])
+async def get_data(current_user: Annotated[User, Depends(get_current_active_user)]):
 
     """Update the current weather data for all cities.
     INPUTS :
         current user : str 
     OUTPUTS : Data updated in Snowflake
     """
-    return {'Message' : 'Not released'}
+    if current_user.get_data == True:
+        print('Identification ok')
+        data_list = await fetch_data()
+        print(data_list)
+
+    return {'Message' : 'Data successfully updated'}
+
+
 
 @app.post("/force_train_model/{city}",  name='Force the train of the model', tags=['Administrators'])
-async def force_data_update(city_data: City, current_user: Annotated[User, Depends(get_current_active_user)]):
+async def force_train_model(city_data: City, current_user: Annotated[User, Depends(get_current_active_user)]):
 
     """Update the model by training the model - Can take some times (training time).
     INPUTS :
@@ -233,8 +317,9 @@ async def force_data_update(city_data: City, current_user: Annotated[User, Depen
     """
     return {'Message' : 'Not released'}
 
+
 @app.post("/forecast_city/{city}",  name='Forecast 7-days', tags=['Backend'])
-async def forecast(city_data: City):
+async def forecast(city_data: City, current_user: Annotated[User, Depends(get_current_active_user)]):
 
     """Returns the forecast of weather feature for city = {city}.
     INPUTS :
