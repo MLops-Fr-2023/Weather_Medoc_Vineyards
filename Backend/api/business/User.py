6f91933372dd73c7c865ef12f8d15ac45290fb73
Joffrey Lemery <joffrey.lemery.jl@gmail.com>
2023-06-05 20:38:14 -0400

    Ajout de quelques regle métier Transformation 'dynamique' de Edit_user

--- Backend/api/business/User.py
+++ Backend/api/business/User.py
@@ -18,3 +18,13 @@ class UserInDB(User, BaseModel):
     pwd_hash: str
 
     
+class UserAdd(BaseModel):
+    user_id: str
+    pwd_hash: Optional[str] = None
+    firstname: Optional[str] = None
+    lastname: Optional[str] = None
+    user_email: Optional[str] = None
+    position: Optional[str] = None
+    #create_date: Optional[date] =  datetime.now().strftime("%Y-%m-%d")
+    #last_upd_date: Optional[date] = datetime.now().strftime("%Y-%m-%d")
+    active: Optional[bool] = True
