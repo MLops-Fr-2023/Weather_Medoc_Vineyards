ab79f34d946e5a738a92257bd629c0e25afa9fc6
nico <nicolas.carayon@gmail.com>
2023-07-17 08:17:59 +0000

    Differentiate MLFlow and AirFlow variables and fix authentication issues

--- Backend/run_docker_compose.sh
+++ Backend/run_docker_compose.sh
@@ -8,9 +8,9 @@ docker volume rm $(docker volume ls -q)
 rm -rf ./airflow/logs
 rm -rf ./airflow/plugins
 
-
 mkdir ./airflow/logs ./airflow/plugins
-sudo echo -e "AIRFLOW_UID=$(id -u)\nAIRFLOW_GID=0" > .env.uid
+source ./.env
+sudo chown -R $USER $AIRFLOW_PGDATA
 
 #sudo docker-compose build --no-cache
 docker-compose build
